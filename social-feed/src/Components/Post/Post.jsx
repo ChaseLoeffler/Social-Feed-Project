9c7908a5b55b02f4bddaa20f1b60de5aa8faeb9f
ChaseLoeffler <chaselo609@gmail.com>
2023-10-20 10:07:58 -0400

    Fleshed out Like and Dislike Button functionailty. Added a Post.css page to Help with button functionailty.

--- social-feed/src/Components/Post/Post.jsx
+++ social-feed/src/Components/Post/Post.jsx
@@ -1,14 +1,44 @@
-
+import React, { useState } from 'react';
+import './Post.css'
 
 const Post = (props) => {
+
+
+    const [likeButtonClass, setLikeButtonClass] = useState();
+    const [dislikeButtonClass, setDislikeButtonClass] = useState();
+
+    function likeButtonClicked(){
+        if(likeButtonClass === "inactive"){
+            setLikeButtonClass("l-active");
+        }
+        else{
+            setLikeButtonClass("inactive");
+        }
+    }
+    function dislikeButtonClicked(){
+        if(dislikeButtonClass === "inactive"){
+            setDislikeButtonClass("d-active");
+        }
+        else{
+            setDislikeButtonClass("inactive");
+        }
+    }
+
+
     return ( 
+        <div>
         <div>
             <header>{props.name}</header>
             <p>{props.comment}</p>
-            <button>Likes</button>
-            <button>Dislikes</button>
+        </div>
+        <div>
+            <button className={likeButtonClass} onClick={likeButtonClicked}><i className='fa fa-thumbs-up'></i></button>
+            <button className={dislikeButtonClass} onClick={dislikeButtonClicked}><i className='fa fa-thumbs-down'></i></button>
+        </div>
+        <hr></hr>
         </div>
      );
 }
  
 export default Post;
+
